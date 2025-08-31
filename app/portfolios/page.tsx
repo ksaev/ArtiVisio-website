6745fc9a67e3a2ba8d820cdf7465fc81d6d77402
KOUASSI SIEBE ADELPHE <kouassisiebe@gmail.com>
2025-08-31 00:20:14 +0000

    update realisations page

--- app/portfolios/page.tsx
+++ app/portfolios/page.tsx
@@ -63,7 +63,7 @@ export default function PortfoliosPage() {
       title: "CV Moderne ",
       client: "Malick Sow",
       category: "cv",
-      image: "/CVPro.jpg",
+      image: "/CVPro.webp",
       description: "CV élégant en anglais pensé pour l’impact international et une présentation professionnelle soignée.",
       link: "/CVPro.webp",
       tags: ["Website", "Consulting", "Landing"],
